11b32e967bc8dd20641e047b2c24e4807bc0e42e
Angelo Rojas <angelo.maeda@hotmail.com>
2024-04-24 16:09:24 -0300

    pipeline_processamento

--- main_pipelines.py
+++ main_pipelines.py
@@ -1,7 +1,13 @@
 from data import comercio_pipeline
+from data import processamento_pipeline
 from const import comercio_url
+from const import processamento_urls
 
 
 if __name__ == '__main__':
 
     print(comercio_pipeline(comercio_url).info())
+
+    for x in processamento_urls:
+        print(processamento_pipeline(x).info())
+    
