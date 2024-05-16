e8eea8c32b851f9633eddcc6520097c123ae8952
AmericoAlves35 <67383209+AmericoAlves35@users.noreply.github.com>
2024-05-16 14:59:12 -0300

    Update pipeline.py
    refatoração do codigo para atender os padrões

--- data/Exportacao/pipeline.py
+++ data/Exportacao/pipeline.py
@@ -1,9 +1,17 @@
+import pandas as pd
+from data.Importacao import nodes
 from pandas import DataFrame
-from data.Exportacao import nodes
+from typing import Dict
 
-
-def exportacao_pipeline(path: str) -> DataFrame:
-    df = nodes.read_from_csv(path)
-    df = nodes.process_export_data(df)
-
-    return df
+def importacao_pipeline(import_datas: Dict[str, str]) -> DataFrame:
+    df_comp = DataFrame() 
+    for nome, path in import_datas.items():
+        df = nodes.read_from_csv(path=path)
+        df = nodes.sum_collumns_with_same_year(df)
+        df = nodes.unpivot_years_columns(df)
+        df = nodes.remove_rows_without_import_value(df)
+        df['type'] = nome # add type column with imported product name
+        df = df.drop('Id', axis=1) # remove Id column
+        df_comp = pd.concat([df_comp, df])
+        
+    return df_comp
