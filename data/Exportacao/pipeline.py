0f5f19cd9053156afa8cbc29ed8c03f5123f3888
AmericoAlves35 <67383209+AmericoAlves35@users.noreply.github.com>
2024-05-16 17:46:35 -0300

    Update pipeline.py
    adaptaçao para exportação

--- data/Exportacao/pipeline.py
+++ data/Exportacao/pipeline.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from data.Importacao import nodes
+from data.Exportacao import nodes
 from pandas import DataFrame
 from typing import Dict
 
-def importacao_pipeline(import_datas: Dict[str, str]) -> DataFrame:
+def exportacao_pipeline(import_datas: Dict[str, str]) -> DataFrame:
     df_comp = DataFrame() 
     for nome, path in import_datas.items():
         df = nodes.read_from_csv(path=path)
