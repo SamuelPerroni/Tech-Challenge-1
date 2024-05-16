ff45ddc521695a4b81a0c40bb0836137067c3c31
Samuel Perroni <samuel_perroni@outlook.com>
2024-05-16 10:30:12 -0300

    alterando tratativas nos dados que possuiam ano repetidos

--- data/Importacao/pipeline.py
+++ data/Importacao/pipeline.py
@@ -1,17 +1,20 @@
 import pandas as pd
 from pandas import DataFrame
+from typing import Dict
+
 from data.Importacao import nodes
-from typing import List
 
 
-def importacao_pipeline(paths: List[str]) -> DataFrame:
+
+def importacao_pipeline(import_datas: Dict[str, str]) -> DataFrame:
     df_comp = DataFrame() 
-    for path in paths:
+    for nome, path in import_datas.items():
         df = nodes.read_from_csv(path=path)
-        df = nodes.sum_collumns_same_year(df)
+        df = nodes.sum_collumns_with_same_year(df)
         df = nodes.unpivot_years_columns(df)
         df = nodes.remove_rows_without_import_value(df)
-        df = df.drop('Id', axis=1)
-        df_comp = pd.concat([df])
+        df['type'] = nome # add type column with imported product name
+        df = df.drop('Id', axis=1) # remove Id column
+        df_comp = pd.concat([df_comp, df])
         
     return df_comp
