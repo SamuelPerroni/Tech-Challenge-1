a65d58920334be494da5e9faa481fd217873b963
Americo <m_eco_21@hotmail.com>
2024-05-18 20:31:19 -0300

    configuração API Eportação

--- api/exportacao/routes.py
+++ api/exportacao/routes.py
@@ -1,5 +1,10 @@
 
-from fastapi import APIRouter, HTTPException, status
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, status, Path, Query
+from sqlalchemy.future import select
+from .schemas import ComercioIn, ComercioOut, ComercioUpdate
+from .models import Comercio
+from ..database import SessionLocal
 
 # rotas de exportacao
 exportacao_router = APIRouter()
