eb3516949001534eeb90f838b33944a20eab5755
Americo <m_eco_21@hotmail.com>
2024-05-18 19:54:14 -0300

    configuração API Eportação

---
+++ api/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+import sqlite3
+
+from .config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL, 
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    future=True,
+    class_=AsyncSession 
+    )
+
+Base = declarative_base()
