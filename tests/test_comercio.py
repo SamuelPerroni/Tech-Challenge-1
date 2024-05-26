918c3700af08a221ff16d63ed0c8123630f4fef7
Lucas F. Silva <lucaslfsls@gmail.com>
2024-05-26 15:09:41 -0300

    changes

--- tests/test_comercio.py
+++ tests/test_comercio.py
@@ -34,11 +34,11 @@ async def test_if_faker_generates_an_object(create_fake_data):
 
 
 @pytest.mark.asyncio
-async def test_if_post_endpoint_returns_status_ok_and_the_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_post_endpoint_returns_status_ok_and_the_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio', json=comercio_data)
+    response = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
     response_data = response.json()
     response_data.pop('id')
     assert response.status_code == 201
@@ -46,11 +46,11 @@ async def test_if_post_endpoint_returns_status_ok_and_the_object(create_test_cli
 
 
 @pytest.mark.asyncio
-async def test_if_object_is_created_from_post_endpoint_by_reaching_get_endpoint_with_data(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_object_is_created_from_post_endpoint_by_reaching_get_endpoint_with_data(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio', json=comercio_data)
+    response = test_client.post('/comercio', json=comercio_data,  headers=jwt_token_fixture)
     response_data = response.json()
 
     response2 = test_client.get(f'/comercio/{response_data['id']}')
@@ -59,12 +59,12 @@ async def test_if_object_is_created_from_post_endpoint_by_reaching_get_endpoint_
     assert response_data == response2_data
 
 @pytest.mark.asyncio
-async def test_if_object_is_duplicated_its_handle_by_the_endpoint(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_object_is_duplicated_its_handle_by_the_endpoint(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    _ = test_client.post('/comercio', json=comercio_data)
-    response_post = test_client.post('/comercio', json=comercio_data)
+    _ = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
+    response_post = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
     
     assert response_post.status_code == 409
 
@@ -79,24 +79,24 @@ async def test_if_get_endpoint_returns_an_empty_list_if_there_are_no_objs_in_db(
 
 
 @pytest.mark.asyncio
-async def test_if_put_endpoint_returns_error_if_no_object_in_database_to_update(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_put_endpoint_returns_error_if_no_object_in_database_to_update(create_test_client, create_and_drop_db_yield_engine, create_fake_data,  jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    response = test_client.put('/comercio/1', json=comercio_data)
+    response = test_client.put('/comercio/1', json=comercio_data, headers=jwt_token_fixture)
     
     assert response.status_code == 404
 
 
 @pytest.mark.asyncio
-async def test_id_put_endpoint_returns_the_object_that_was_updated(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_id_put_endpoint_returns_the_object_that_was_updated(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
     comercio_data2 = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio', json=comercio_data)
+    response = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
     id = response.json()['id']
-    response = test_client.put(f'/comercio/{id}', json=comercio_data2)
+    response = test_client.put(f'/comercio/{id}', json=comercio_data2, headers=jwt_token_fixture)
     response_data = response.json()
     response_data.pop('id')
 
@@ -105,14 +105,14 @@ async def test_id_put_endpoint_returns_the_object_that_was_updated(create_test_c
 
 
 @pytest.mark.asyncio
-async def test_if_object_was_updated_in_database_by_put_endpoint(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_object_was_updated_in_database_by_put_endpoint(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
     comercio_data2 = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio', json=comercio_data)
+    response = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
     id = response.json()['id']
-    response = test_client.put(f'/comercio/{id}', json=comercio_data2)
+    response = test_client.put(f'/comercio/{id}', json=comercio_data2, headers=jwt_token_fixture)
     response_data_put = response.json()
 
     response = test_client.get('/comercio')
@@ -122,36 +122,36 @@ async def test_if_object_was_updated_in_database_by_put_endpoint(create_test_cli
 
 
 @pytest.mark.asyncio
-async def test_if_delete_endpoint_returns_status_ok(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_delete_endpoint_returns_status_ok(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio', json=comercio_data)
+    response = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
     id = response.json()['id']
 
-    response = test_client.delete(f'/comercio/{id}')
+    response = test_client.delete(f'/comercio/{id}', headers=jwt_token_fixture)
 
     assert response.status_code == 200 
 
 
 @pytest.mark.asyncio
-async def test_if_delete_endpoint_returns_status_404_if_no_data_to_delete(create_test_client, create_and_drop_db_yield_engine):
+async def test_if_delete_endpoint_returns_status_404_if_no_data_to_delete(create_test_client, create_and_drop_db_yield_engine,  jwt_token_fixture):
     test_client = create_test_client
-    response = test_client.delete(f'/comercio/{1}')
+    response = test_client.delete(f'/comercio/{1}', headers=jwt_token_fixture)
 
     assert response.status_code == 404
 
 
 @pytest.mark.asyncio
-async def test_if_delete_endpoint_deletes_the_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_delete_endpoint_deletes_the_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio', json=comercio_data)
+    response = test_client.post('/comercio', json=comercio_data, headers=jwt_token_fixture)
     response_data = response.json()
     id = response_data['id']
 
-    delete_response = test_client.delete(f'/comercio/{id}')
+    delete_response = test_client.delete(f'/comercio/{id}', headers=jwt_token_fixture)
 
     response = test_client.get('/comercio')
     response_data_get = response.json()
@@ -161,11 +161,11 @@ async def test_if_delete_endpoint_deletes_the_object(create_test_client, create_
 
 
 @pytest.mark.asyncio
-async def test_if_get_or_create_creates_an_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_get_or_create_creates_an_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
 
-    response = test_client.post('/comercio/get-or-create/', json=comercio_data)
+    response = test_client.post('/comercio/get-or-create/', json=comercio_data, headers=jwt_token_fixture)
     response_data = response.json()
 
     response2 = test_client.get(f'/comercio/{response_data['id']}')
@@ -174,15 +174,15 @@ async def test_if_get_or_create_creates_an_object(create_test_client, create_and
     assert response_data == response2_data
 
 @pytest.mark.asyncio
-async def test_if_create_or_update_updates_an_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data):
+async def test_if_create_or_update_updates_an_object(create_test_client, create_and_drop_db_yield_engine, create_fake_data, jwt_token_fixture):
     test_client = create_test_client
     comercio_data = create_fake_data().model_dump()
     comercio_data2 = comercio_data.copy()
     comercio_data2['commerce'] = 0.0
 
-    _ = test_client.post('/comercio/create-or-update/', json=comercio_data)
+    _ = test_client.post('/comercio/create-or-update/', json=comercio_data, headers=jwt_token_fixture)
 
-    response = test_client.post('/comercio/create-or-update/', json=comercio_data2)
+    response = test_client.post('/comercio/create-or-update/', json=comercio_data2, headers=jwt_token_fixture)
     response_data = response.json()
 
     response2 = test_client.get(f'/comercio/{response_data['id']}')
@@ -194,12 +194,12 @@ async def test_if_create_or_update_updates_an_object(create_test_client, create_
 
 
 @pytest.mark.asyncio
-async def test_if_post_endpoint_addd_all_data(create_test_client, create_and_drop_db_yield_engine, run_pipeline):
+async def test_if_post_endpoint_addd_all_data(create_test_client, create_and_drop_db_yield_engine, run_pipeline, jwt_token_fixture):
     test_client = create_test_client
     data = run_pipeline
     responses = []
     for i in data.to_dict('records'):
-        response = test_client.post('/comercio/', json=i)
+        response = test_client.post('/comercio/', json=i, headers=jwt_token_fixture)
         responses.append(response.status_code)
     
     assert all(element == 201 for element in responses) is True
